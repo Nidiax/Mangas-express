3820b1e8d56f1f9f28e8cf72f2a06db1a9cd4bc2
Nidiax <nidiaavila000@gmail.com>
2022-11-29 15:44:25 -0600

    se agrego un boton para poder descargar un archivo pdf

--- MangaExpressController/Areas/Identity/Pages/AdminP/MangasP/Index.cshtml.cs
+++ MangaExpressController/Areas/Identity/Pages/AdminP/MangasP/Index.cshtml.cs
@@ -7,15 +7,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Manga.Models;
 using MangaExpressController.Data;
+using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace MangaExpressController.Areas.Identity.Pages.AdminP.MangasP
 {
     public class IndexModel : PageModel
     {
         private readonly MangaExpressController.Data.ApplicationDbContext _context;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public IndexModel(MangaExpressController.Data.ApplicationDbContext context)
+        public IndexModel(MangaExpressController.Data.ApplicationDbContext context, IHostingEnvironment hostingEnvironment)
         {
+            _hostingEnvironment= hostingEnvironment;
             _context = context;
         }
 
@@ -41,6 +47,20 @@ namespace MangaExpressController.Areas.Identity.Pages.AdminP.MangasP
             _context.SaveChanges();
             await OnGetAsync();
         }
+
+        //public async Task<IActionResult> OnGetPDFAsync() {
+        //    MangaM = await _context.Mangas.ToListAsync();
+        //    HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+        //    WebKitConverterSettings settings = new WebKitConverterSettings();
+        //    //Set WebKit path
+        //    settings.WebKitPath = Path.Combine(_hostingEnvironment.ContentRootPath, "QtBinariesWindows");
+        //    //Assign WebKit settings to HTML converter
+        //    htmlConverter.ConverterSettings = settings;
+        //    PdfDocument document = htmlConverter.Convert("https://localhost:44324/Identity/AdminP/MangasP");
+        //    MemoryStream stream = new MemoryStream();
+        //    document.Save(stream);
+        //    return File(stream.ToArray(), System.Net.Mime.MediaTypeNames.Application.Pdf, "Sample.pdf");
+        //}
     }
 
 }
